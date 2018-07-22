a79d3917af9c89ab0293a46a7a16b6c199221fd5
muhiddin <turgunovm@yahoo.com>
2018-07-22 19:18:13 +0500

    project setup is done

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp         = require("gulp"),
+    sass         = require("gulp-sass"),
+    plumber      = require("gulp-plumber"),
+    postcss      = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    server       = require("browser-sync").create(),
+    reload       = server.reload,
+    mqpacker     = require("css-mqpacker"),
+    rename       = require("gulp-rename");
+
+
+  /* Compiling Sass files */
+    gulp.task("style", function() {
+
+      gulp.src("assets/sass/**/*.scss")
+        .pipe(plumber())
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(postcss([
+          autoprefixer({
+            browsers: ["last 2 versions"],
+            cascade: false
+          }),
+          mqpacker({ sort: true })
+        ]))
+        .pipe(gulp.dest("assets/css"))
+        .pipe(reload({ stream: true }));
+
+    });
+
+
+
+  /* Launching Server */
+    gulp.task("serve", ["style"], function() {
+
+      server.init({
+        server: {
+          baseDir: "."
+        },
+        notify: false
+      });
+
+
+      gulp.watch("assets/sass/**/*.scss", ["style"]);
+      gulp.watch("*.html").on("change", reload);
+      gulp.watch("pages/**/*.html").on("change", reload);
+      gulp.watch('assets/js/**/*.js').on("change", reload);
+
+    });
+
+
+  /* Defaul Gulp task */
+    gulp.task("default", ["serve"]);
