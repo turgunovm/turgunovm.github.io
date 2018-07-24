f249a0648b4dabdc55deb0cd6cc9120a1563c9ad
muhiddin <turgunovm@yahoo.com>
2018-07-24 16:22:24 +0500

    Fix mediaqueries

---
+++ assets/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  // $( window ).resize(function() {
+  //
+  //   var width = $(window).width();
+  //   if(width <= 768) {
+  //     console.log("i've said you dont resize!");
+  //   }
+  //
+  // });
+
+});
