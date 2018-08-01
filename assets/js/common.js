c5ff71084497aa39ec49064667cdcaa0c415d0e4
muhiddin <turgunovm@yahoo.com>
2018-08-01 10:49:05 +0500

    just did something big

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,15 +1,23 @@
 function hideLoadingLayer(){
 document.getElementById("loading_layer").style.visibility="hidden";
 }
-$(document).ready(function() {
 
-  // $( window ).resize(function() {
-  //
-  //   var width = $(window).width();
-  //   if(width <= 768) {
-  //     console.log("i've said you dont resize!");
-  //   }
-  //
-  // });
+$('.menu__link').on('click', function(e) {
 
+	e.preventDefault;
+	$(this).toggleClass('menu__link--active');
+	$('.nav').toggleClass('visible');
+	$('.masthead').on('click', function() {
+		removeClass('.menu__link--active');
+	});
 });
+
+var typed = new Typed('.typed', {
+    strings: [' ', '<strong>Designer</strong>', '<strong>Muhiddin</strong>'],
+    typeSpeed: 100,
+		startDelay: 1000,
+		cursorChar: '_',
+    backSpeed: 80,
+		smartBackspace: false,
+    loop: false
+  });
